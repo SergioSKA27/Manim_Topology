6ef933e4bbc5b1ef10e914c580acce1d0840a185
Sergio Lopez Martinez <sergioska81@comunidad.unam.mx>
2023-09-23 22:15:30 -0600

    First 2 pages

--- pages/knotsdef.py
+++ pages/knotsdef.py
@@ -351,4 +351,171 @@ st.markdown('''
 </div>
 ''',unsafe_allow_html=True)
 
-sac.divider(label='', icon='backpack', align='center')
+sac.divider(label='', icon='balloon-heart', align='center')
+
+
+
+cols = st.columns([.5,.5])
+
+with cols[0]:
+    st.header('Disco $D^n$')
+    st.divider()
+    r'''
+**Definición:** El $n$-disco, denotado como $D^n$, se define como el conjunto de puntos en $\mathbb{R}^n$ que satisfacen
+la siguiente condición:
+
+$$
+D^n = \{ x \in \mathbb{R}^n \mid \|x\| \leq 1 \}
+$$
+'''
+
+with cols[1]:
+    tabs = st.tabs(['Discos $D^0,D^1,D^2$','Discos $D^3$'])
+    with tabs[0]:
+        st.video('Videos/PAGINA2/Disco.mp4')
+
+    with tabs[1]:
+        st.video('Videos/PAGINA2/Disco3.mp4')
+
+
+sac.divider(label='', icon='balloon-heart', align='center',key='div1')
+
+cols1 = st.columns([.5,.5])
+
+with cols1[0]:
+    st.header('Esfera $S^{n-1}$')
+    st.divider()
+    r'''
+**Definición:** La esfera $S^{n-1}$ en el $n$-disco $D^n$ se define como el conjunto de puntos en el $n$-disco $D^n$ que satisfacen la siguiente condición:
+
+$$
+S^{n-1} = \{ x \in D^n \mid \|x\| = 1 \}  = \partial D^n
+$$
+
+'''
+
+with cols1[1]:
+    tabs1 = st.tabs(['Esferas $S^0,S^1$','Esfera $S^2$', 'Esfera $S^2$ (Ejemplo 2)'])
+    with tabs1[0]:
+        st.video('Videos/PAGINA2/Esfera.mp4')
+
+    with tabs1[1]:
+        st.video('Videos/PAGINA2/Esfera2D.mp4')
+
+    with tabs1[2]:
+        st.video('Videos/PAGINA2/Esfera2Example2.mp4')
+
+
+sac.divider(label='', icon='balloon-heart', align='center',key='div2')
+
+cols2 = st.columns([.5,.5])
+
+with cols2[0]:
+    st.header('Nudos')
+    st.divider()
+    r'''
+**Definició$n_1$ :** Un **nudo** es una **inmersión continua** de la
+circunferencia estándar $S^1$ en el espacio tridimensional $\mathbb{R}^3$. En otras palabras, un nudo es
+una función continua $f : S^1 \to \mathbb{R}^3$ que asigna cada punto en la circunferencia $S^1$ a un punto
+en el espacio tridimensional $\mathbb{R}^3$, de manera que la función es inyectiva (no se cruza a sí misma) y
+su imagen es una curva cerrada y simple en $\mathbb{R}^3$.
+
+**Definició$n_2$ :** Un **nudo** es un subconjunto $K$ del espacio tridimensional
+euclidiano $\mathbb{R}^3$ que es **homeomorfo** a la circunferencia estándar $S^1$, es decir, existe un
+homeomorfismo $\varphi : S^1 \to K$ entre la circunferencia $S^1$ y el conjunto $K$.
+
+
+
+'''
+
+with cols2[1]:
+    ''''''
+
+
+
+sac.divider(label='', icon='balloon-heart', align='center',key='div3')
+
+cols3 = st.columns([.5,.5])
+
+with cols3[0]:
+    st.header('Nudos Semejantes')
+    st.divider()
+    r'''
+**Definición:** Dos nudos $K_1$ y $K_2$ son semejantes si existe un homeomorfismo
+$$
+h : \mathbb{R}^3 \to \mathbb{R}^3
+$$
+tal que  $h(K_1) = K_2$.
+
+
+En otras palabras, dos nudos son semejantes si pueden ser deformados continuamente uno en el otro sin romper ni cruzar la estructura de nudo en ningún momento. Esto significa que los nudos semejantes tienen la misma topología y se diferencian solo por su forma tridimensional.
+
+'''
+
+with cols3[1]:
+    ''''''
+
+
+
+
+
+sac.divider(label='', icon='balloon-heart', align='center',key='div4')
+
+cols4 = st.columns([.5,.5])
+
+with cols4[0]:
+    st.header('¿Por qué $\mathbb{R}^3$?')
+    st.divider()
+    r'''
+Uno podría preguntarse por qué se define un nudo como un subconjunto del espacio tridimensional $\mathbb{R}^3$.
+¿Únicamente debido a que vivimos en un mundo tridimensional? ¿Por qué no definir un nudo $K$ como un subconjunto del
+espacio $\mathbb{R}^n$ que sea homeomorfo a $S^1$? Debemos notar que $n$ debe ser mayor o igual a dos, ya que
+cualquier aplicación continua de la circunferencia en la recta real manda al menos un par de puntos diametralmente
+opuestos al mismo punto y, por lo tanto, no puede haber subconjuntos de $\mathbb{R}$ que sean homeomorfos a $S^1$.
+Sin embargo, si $n \neq 3$, existe un homeomorfismo $h : \mathbb{R}^n \rightarrow \mathbb{R}^n$ tal que $h(K)$ es
+la circunferencia estándar. Para $n = 2$, esto constituye el famoso Teorema de Schönflies; para $n \geq 4$,
+el resultado nos dice que si viviéramos en un mundo de 4 o más dimensiones, podríamos desanudar todos los nudos.
+'''
+
+with cols4[1]:
+    st.header('Teorema de Schönflies')
+    st.divider()
+    r'''
+
+**Teorema:** Sea $C$ una curva cerrada simple en el plano euclidiano $\mathbb{R}^2$. Entonces, existe un homeomorfismo
+entre el conjunto $C$ y el círculo estándar $S^1$, es decir, existe un homeomorfismo $h : C \to S^1$.
+
+En otras palabras, el Teorema de Schönflies establece que cualquier curva cerrada simple en el plano puede ser
+transformada mediante un homeomorfismo en el círculo estándar. Esto implica que las curvas cerradas simples en
+el plano son topológicamente equivalentes al círculo y pueden ser "desenrolladas" o "transformadas" en una forma
+estándar de círculo mediante una función continua.
+'''
+
+sac.divider(label='', icon='balloon-heart', align='center',key='div5')
+
+cols5 = st.columns([.5,.5])
+
+with cols5[0]:
+    st.header('Nudos Equivalentes')
+    st.divider()
+    r'''
+**Definición.** Dos nudos $K_1$ y $K_2$ son equivalentes si existe un homeomorfismo
+
+$$
+h : \mathbb{R}^3 \rightarrow \mathbb{R}^3
+$$
+que preserva la orientación, es decir, $h$ es una función continua y
+biyectiva con una inversa continua, y además $h(K_1) = K_2$. La equivalencia de nudos se denotará como $K_1 \sim K_2$.
+
+El concepto de nudos equivalentes coincide con el concepto físico de igualdad de nudos.
+De hecho, se puede demostrar que dos nudos $K_1$ y $K_2$ son equivalentes si y solo si existe un homeomorfismo
+$h : \mathbb{R}^3 \rightarrow \mathbb{R}^3$ y un número real $k > 0$ tal que $h(K_1) = K_2$ y $h(x) = x$ siempre que
+$\|x\| \geq k$.
+
+'''
+
+with cols5[1]:
+    ''''''
+
+
+
